acddfbcdca2b10af5a20aa8230b58435e155ebd9
Dan Connor <danconn@danconnor.com>
2016-03-27 15:38:12 -0400

    handle module names like AdminUser by snake-casing to admin_user

--- lib/crudblood/model.ex
+++ lib/crudblood/model.ex
@@ -6,10 +6,13 @@ defmodule Crudblood.Model do
       end
 
       def name do
-        "#{__MODULE__}" |>
-          String.split(".") |>
-          List.last |>
-          String.downcase
+        "#{__MODULE__}"
+        |> String.split(".")
+        |> List.last                                          # take the "AdminUser" from "MyApp.AdminUser"
+        |> String.replace(~r/([A-Z]+)([A-Z][a-z])/,"\\1_\\2") # ... and snake case it (ex: "admin_user")
+        |> String.replace(~r/([a-z\d])([A-Z])/,"\\1_\\2")
+        |> String.replace("-", "_")
+        |> String.downcase
       end
 
       def plural_name do
